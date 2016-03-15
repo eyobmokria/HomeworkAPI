48b12a25282bc4b894b9937ceab7ef05301c0af5
eyob <eyob.mk@gmail.com>
2016-03-15 15:12:37 -0500

    hi

---
+++ src/test/AllTests.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * @author eyob mokria
+ *
+ */
+@RunWith(Suite.class)
+@SuiteClasses({})
+public class AllTests {
+
+}
